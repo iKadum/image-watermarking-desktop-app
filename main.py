8c4b97393a1b7f9c90f921c029edf7e623fab1e2
Igor Kadum <igor.kadum@gmail.com>
2022-01-21 00:52:15 +0100

    Add text color selector

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 TRANSPARENCY = 180  # 0 (full transparent) - 255 (not transparent)
-R, G, B = (0, 0, 0)  # black
+# R, G, B = (0, 0, 0)  # black
+COLORS = {"black": (0, 0, 0),
+          "white": (255, 255, 255)}
 
 
 def img_opacity(image_object, level):
@@ -20,25 +22,25 @@ def img_opacity(image_object, level):
     new_data = []
     for item in data:
         if item[3] != 0:  # if pixel is not transparent (alpha channel is not 0)
-            r, g, b, a = item
-            new_data.append((r, g, b, level))  # change only alpha channel
+            r_, g_, b_, a_ = item
+            new_data.append((r_, g_, b_, level))  # change only alpha channel
         else:
             new_data.append(item)
     image_object.putdata(new_data)
 
 
-def text_watermark(image_path, watermark_text, output_path, text_color=(R, G, B, TRANSPARENCY)):
+def text_watermark(image_path, wm_text, output_path, wm_text_color):
     im = Image.open(image_path).convert("RGBA")  # open image and convert to have alpha channel
     txt_im = Image.new("RGBA", im.size, (0, 0, 0, 0))  # new transparent image of same size for text
     width, height = txt_im.size
     draw = ImageDraw.Draw(txt_im)
     font = ImageFont.truetype("arial.ttf", height // 10)
-    txt_width, txt_height = draw.textsize(watermark_text, font)
+    txt_width, txt_height = draw.textsize(wm_text, font)
     margin = height // 30
     x = width - txt_width - margin
     y = height - txt_height - margin
     coordinates = (x, y)
-    draw.text(coordinates, watermark_text, text_color, font=font)
+    draw.text(coordinates, wm_text, wm_text_color, font=font)
 
     watermarked_im = Image.alpha_composite(im, txt_im)  # combine 2 images
     watermarked_im = watermarked_im.convert("RGB")
@@ -87,19 +89,30 @@ def radio_used():
         label_watermark_image.configure(state="normal")
         label_watermark_text.configure(state="disabled")
         entry_watermark_text.configure(state="disabled")
+        text_color_listbox.configure(state="disabled")
     else:
         label_choose_watermark.configure(state="disabled")
         select_watermark_button.configure(state="disabled")
         label_watermark_image.configure(state="disabled")
         label_watermark_text.configure(state="normal")
         entry_watermark_text.configure(state="normal")
+        text_color_listbox.configure(state="normal")
+
+
+def text_color_listbox_used(event):
+    global r
+    global g
+    global b
+    text_color = text_color_listbox.get(text_color_listbox.curselection())
+    r, g, b = (COLORS[text_color])
+    return event  # this line is only to "use" event so the code has no warnings :)
 
 
 def watermark_func():
     try:
         option = radio_state.get()
         if option == "text":
-            text_watermark(image, entry_watermark_text.get(), output_image)
+            text_watermark(image, entry_watermark_text.get(), output_image, (r, g, b, a))
         else:
             image_watermark(image, watermark_image, output_image)
 
@@ -132,28 +145,38 @@ label_watermark_text = tk.Label(text="Enter Watermark Text: ")
 label_watermark_text.grid(column=0, row=4, sticky="W")
 
 entry_watermark_text = tk.Entry()
-entry_watermark_text.grid(column=0, row=5, sticky="W", pady=(0, 15))
+entry_watermark_text.grid(column=0, row=5, sticky="W")
+
+text_color_listbox = tk.Listbox()
+i = 0
+for color in COLORS:
+    text_color_listbox.insert(i, color)
+    i += 1
+text_color_listbox.config(height=i)
+text_color_listbox.bind("<<ListboxSelect>>", text_color_listbox_used)
+text_color_listbox.grid(column=0, row=6, sticky="W", pady=(0, 15))
 
 radiobutton_image = tk.Radiobutton(text="Image Watermark", value="image", variable=radio_state, command=radio_used)
-radiobutton_image.grid(column=0, row=6, sticky="W")
+radiobutton_image.grid(column=0, row=7, sticky="W")
 
 label_choose_watermark = tk.Label(text="Choose watermark image: none")
-label_choose_watermark.grid(column=0, row=7, sticky="W")
+label_choose_watermark.grid(column=0, row=8, sticky="W")
 label_choose_watermark.configure(state="disabled")
 
 select_watermark_button = tk.Button(text="Browse images", command=select_watermark_image)
-select_watermark_button.grid(column=0, row=8, sticky="W")
+select_watermark_button.grid(column=0, row=9, sticky="W")
 select_watermark_button.configure(state="disabled")
 
 label_watermark_image = tk.Label(text="Chosen image: none")
-label_watermark_image.grid(column=0, row=9, sticky="W", pady=(0, 30))
+label_watermark_image.grid(column=0, row=10, sticky="W", pady=(0, 30))
 label_watermark_image.configure(state="disabled")
 
 watermark_button = tk.Button(text="Watermark the selected image!", command=watermark_func)
-watermark_button.grid(column=0, row=10,  sticky="W", pady=(0, 30))
+watermark_button.grid(column=0, row=11,  sticky="W", pady=(0, 30))
 
 image = ""
 watermark_image = ""
 output_image = "watermark.jpg"
+r, g, b, a = (0, 0, 0, 255)
 
 window.mainloop()
