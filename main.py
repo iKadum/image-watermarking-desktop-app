92e1ad843cd641e8b66597f55754c4e2e720dac1
Igor Kadum <igor.kadum@gmail.com>
2022-01-21 03:42:20 +0100

    Add transparency (opacity) dropdown menu

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from tkinter import messagebox
 from PIL import Image, ImageDraw, ImageFont
 
 
-TRANSPARENCY = 180  # 0 (full transparent) - 255 (not transparent)
-# R, G, B = (0, 0, 0)  # black
 COLORS = {"black": (0, 0, 0),
           "white": (255, 255, 255),
           "blue": (0, 0, 255),
@@ -57,7 +55,7 @@ def image_watermark(image_path, watermark_path, output_path):
     wm_im = Image.new("RGBA", im.size, (0, 0, 0, 0))  # new transparent image of same size for watermark
     width, height = wm_im.size
     watermark = Image.open(watermark_path).convert("RGBA")  # open watermark image and convert to have alpha channel
-    img_opacity(watermark, TRANSPARENCY)
+    img_opacity(watermark, a)
     wm_width, wm_height = watermark.size
     margin = height // 30
     x = width - wm_width - margin
@@ -105,10 +103,10 @@ def radio_used():
 
 
 def watermark_func():
-    global r
-    global g
-    global b
+    global r, g, b, a
     text_color = selected_color.get()
+    opacity = selected_opacity.get()
+    a = int(int(opacity) / 100 * 255)
     r, g, b = (COLORS[text_color])
     try:
         option = radio_state.get()
@@ -127,15 +125,12 @@ window.title("Image Watermarking")
 window.minsize(width=600, height=0)
 window.config(pady=30, padx=45)
 
-label_choose = tk.Label(text="Choose an image: ")
-label_choose.grid(column=0, row=0, sticky="W", pady=(0, 3))
+tk.Label(text="Choose an image: ").grid(column=0, row=0, sticky="W", pady=(0, 3))
 
-select_button = tk.Button(text="Browse images", command=select_image)
-select_button.grid(column=0, row=1, sticky="W")
+tk.Button(text="Browse images", command=select_image).grid(column=0, row=1, sticky="W")
 
 label_image = tk.Label(text="Chosen image: none")
 label_image.grid(column=0, row=2, sticky="W", pady=(0, 15))
-
 
 radio_state = tk.StringVar()
 radio_state.set("text")
@@ -148,31 +143,43 @@ label_watermark_text.grid(column=0, row=4, sticky="W")
 entry_watermark_text = tk.Entry()
 entry_watermark_text.grid(column=0, row=5, sticky="W")
 
+tk.Label(text="Choose text color: ").grid(column=0, row=6, sticky="W")
+
 colors = []
 for color in COLORS:
     colors.append(color)
 selected_color = tk.StringVar()
 selected_color.set("black")
 text_color_menu = tk.OptionMenu(window, selected_color, *colors)
-text_color_menu.grid(column=0, row=6, sticky="W", pady=(0, 15))
+text_color_menu.grid(column=0, row=7, sticky="W", pady=(0, 15))
 
 radiobutton_image = tk.Radiobutton(text="Image Watermark", value="image", variable=radio_state, command=radio_used)
-radiobutton_image.grid(column=0, row=7, sticky="W")
+radiobutton_image.grid(column=0, row=8, sticky="W")
 
 label_choose_watermark = tk.Label(text="Choose watermark image: none")
-label_choose_watermark.grid(column=0, row=8, sticky="W")
+label_choose_watermark.grid(column=0, row=9, sticky="W")
 label_choose_watermark.configure(state="disabled")
 
 select_watermark_button = tk.Button(text="Browse images", command=select_watermark_image)
-select_watermark_button.grid(column=0, row=9, sticky="W")
+select_watermark_button.grid(column=0, row=10, sticky="W")
 select_watermark_button.configure(state="disabled")
 
 label_watermark_image = tk.Label(text="Chosen image: none")
-label_watermark_image.grid(column=0, row=10, sticky="W", pady=(0, 30))
+label_watermark_image.grid(column=0, row=11, sticky="W", pady=(0, 30))
 label_watermark_image.configure(state="disabled")
 
+tk.Label(text="Chose watermark opacity: ").grid(column=0, row=12, sticky="W")
+
+opacity_steps = []
+for step in range(10, 101, 10):
+    opacity_steps.append(step)
+selected_opacity = tk.StringVar()
+selected_opacity.set(90)
+opacity_menu = tk.OptionMenu(window, selected_opacity, *opacity_steps)
+opacity_menu.grid(column=0, row=13, sticky="W", pady=(0, 15))
+
 watermark_button = tk.Button(text="Watermark the selected image!", command=watermark_func)
-watermark_button.grid(column=0, row=11,  sticky="W", pady=(0, 30))
+watermark_button.grid(column=0, row=14,  sticky="W", pady=(0, 30))
 
 image = ""
 watermark_image = ""
